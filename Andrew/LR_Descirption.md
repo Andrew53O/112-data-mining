f07e4efc35bce8b139d708d50191b868462fdfa5
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2024-04-23 14:48:14 +0800

    adding Logistic Regression

---
+++ Andrew/LR_Descirption.md
@@ -0,0 +1,17 @@
+.values -> return numpy reprsentation of pandas dataframe
+
+learning_rate = 0.0000015 -> 63 %
+learning_rate = 0.000015 -> 65 %
+learning_rate = 0.00015 -> 68%
+learning_rate = 0.00025 -> 69% 
+learning_rate = 0.00027 -> 70%
+
+
+80%
+iterations = 10000000
+learning_rate = 0.00027
+
+![LogisticRegression](image.png)
+
+![LogisticREgression2](image-1.png)
+![LogsiticREgreesion3](image-2.png)
---
+++ Andrew/Logistic_Regression.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data from CSV file
+Train_data = pd.read_csv('Data/B/train_data.csv')
+Test_data = pd.read_csv('Data/B/test_data.csv')
+
+# Split to parameters and outcome data 
+X_train = Train_data.iloc[:, :7].values.T # Transpose data
+Y_train = Train_data.iloc[:, 8].values.reshape(1, X_train.shape[1]) # Reshape data to (1, n)
+X_test = Test_data.iloc[:, :7].values.T
+Y_test = Test_data.iloc[:, 8].values.reshape(1, X_test.shape[1])
+
+# Initialize parameters
+iterate_count = 10000
+lrate = 0.00027
+
+# Sigmoid function
+def sigmoid(x):
+    if np.any(x < -100): # if x is too negative 
+        return np.where(x < -100, 0, 1 / (1 + np.exp(-x)))
+    else:
+        return 1 / (1 + np.exp(-x))
+
+def Logistic_Regression(X, Y, lrate, iterate_count):
+    m = X_train.shape[1]
+    n = X_train.shape[0]
+    
+    Weight = np.zeros((n,1)) # numpy array filled with zero
+    Bias = 0
+    cost_all = []
+    
+    for _ in range(iterate_count):
+        # Calculate the sigmoid
+        Z = np.dot(Weight.T, X) + Bias
+        A = sigmoid(Z)
+        
+        # Cost function -> Error representation
+        cost = -(1/m)*np.sum(Y * np.log(A + 1e-9) + (1-Y) * np.log(1 - A + 1e-9))
+        
+        # Gradient Descent
+        dWeight = (1/m) * np.dot(A-Y, X.T)
+        dBias = (1/m) * np.sum(A - Y)
+        
+        Weight = Weight - lrate * dWeight.T
+        Bias = Bias - lrate * dBias
+        
+        # Keeping track of our cost function value
+        cost_all.append(cost)
+        
+    return Weight, Bias, cost_all
+
+Weight, Bias, cost_all = Logistic_Regression(X_train, Y_train, lrate = lrate, iterate_count = iterate_count)
+
+# Plotting the cost function
+plt.plot(np.arange(iterate_count), cost_all)
+plt.show()
+
+def accuracy(X, Actual_value, Weight, Bias):
+    # Calculate the sigmoid
+    Z = np.dot(Weight.T, X) + Bias
+    Predicted_value = sigmoid(Z)
+    Predicted_value = np.array(Predicted_value > 0.5, dtype='int64') # Change to 0 or 1
+        
+    acc = (1 - np.sum(np.absolute(Predicted_value - Actual_value))/Actual_value.shape[1])*100
+    
+    print("Accuracy: ", round(acc, 2), "%")
+    
+accuracy(X_test, Y_test, Weight, Bias)
