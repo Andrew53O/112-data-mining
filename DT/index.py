4b0bce636576034afd52e9e3fa8f24c0716f6d86
陳育霖_Leo <75311991+wendy062644@users.noreply.github.com>
2024-04-23 22:09:55 +0800

    Update Decision Tree

--- DT/index.py
+++ DT/index.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 
 train_data = pd.read_csv('A/train_data.csv')
-
 test_data = pd.read_csv('A/test_data.csv')
 
 X_train = train_data.drop('Outcome', axis=1)
@@ -12,17 +11,11 @@ Outcome_train = train_data['Outcome']
 X_test = test_data.drop('Outcome', axis=1)
 Outcome_test = test_data['Outcome']
 
-total = 0 # 總正確率
-times = 10 # 計算次數
+# 決策樹
+clf = DecisionTreeClassifier()
+clf.fit(X_train, Outcome_train)
 
-for i in range(times):
-    # 決策樹
-    clf = DecisionTreeClassifier()
-    clf.fit(X_train, Outcome_train)
+# 預測
+y_pred = clf.predict(X_test)
 
-    # 預測
-    y_pred = clf.predict(X_test)
-
-    total += np.sum(Outcome_test == y_pred) / len(y_pred)
-
-print(f"Average Accuracy in {times} times: {total * 100 / times:.2f}%")
+print(f"Accuracy: {np.sum(y_pred == Outcome_test) / len(Outcome_test) * 100:.2f}%")
