f4322915bab900e6a54db1271bbedc0adb30a2bb
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2024-05-30 23:38:43 +0800

    adding large size

--- FinalTerm/Andrew/dbscan.py
+++ FinalTerm/Andrew/dbscan.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-train_data = pd.read_csv('data/train_data.csv', index_col='id')
-train_labels = pd.read_csv('data/train_label.csv', index_col='id')
-test_data = pd.read_csv('data/test_data.csv', index_col='id')
-test_labels = pd.read_csv('data/test_label.csv', index_col='id')
+train_data = pd.read_csv('Data/train_data.csv', index_col='id')
+train_labels = pd.read_csv('Data/train_label.csv', index_col='id')
+test_data = pd.read_csv('Data/test_data.csv', index_col='id')
+test_labels = pd.read_csv('Data/test_label.csv', index_col='id')
