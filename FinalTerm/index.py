8a59d399fff28ab991b103d436ef8105453d19dd
陳育霖_Leo <75311991+wendy062644@users.noreply.github.com>
2024-05-31 16:36:49 +0800

    Update

--- FinalTerm/index.py
+++ FinalTerm/index.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cluster import KMeans
 from sklearn.impute import KNNImputer
 
 # 加载训练集和测试集数据
@@ -10,33 +9,47 @@ train_labels = pd.read_csv('data/train_label.csv', index_col='id')
 test_data = pd.read_csv('data/test_data.csv', index_col='id')
 test_labels = pd.read_csv('data/test_label.csv', index_col='id')
 
-def fill_zeros_with_knn(data, n_neighbors=5):
-    # 记录哪些列是全零的
-    zero_cols = data.columns[(data == 0).all()]
-    non_zero_data = data.drop(columns=zero_cols)
-    
-    imputer = KNNImputer(n_neighbors=n_neighbors, missing_values=0)
-    data_imputed = imputer.fit_transform(non_zero_data)
-    
-    # 将填补后的数据转换回 DataFrame
-    data_imputed_df = pd.DataFrame(data_imputed, columns=non_zero_data.columns, index=data.index)
-    
-    # 创建一个全零的 DataFrame
-    zero_cols_df = pd.DataFrame(0, index=data.index, columns=zero_cols)
-    
-    # 将全零的列加回去
-    data_imputed_df = pd.concat([data_imputed_df, zero_cols_df], axis=1)
-    
-    # 按照原始列的顺序重新排序
-    data_imputed_df = data_imputed_df.reindex(columns=data.columns)
-    
-    return data_imputed_df
+def compute_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
 
-# 填补训练数据中的0值
-#train_data = fill_zeros_with_knn(train_data)
+# 定义K-means算法
+class MyKMeans:
+    def __init__(self, n_clusters, max_iters=100, random_state=42):
+        self.n_clusters = n_clusters
+        self.max_iters = max_iters
+        self.random_state = random_state
 
-# 填补测试数据中的0值
-#test_data = fill_zeros_with_knn(test_data)
+    def fit(self, X):
+        np.random.seed(self.random_state)
+        random_indices = np.random.permutation(X.shape[0])
+        self.centroids = X[random_indices[:self.n_clusters]]
+
+        for _ in range(self.max_iters):
+            self.labels = self.assign_clusters(X)
+            old_centroids = self.centroids
+            self.centroids = self.update_centroids(X)
+
+            if np.all(old_centroids == self.centroids):
+                break
+
+    def assign_clusters(self, X):
+        labels = np.zeros(X.shape[0])
+        for i, point in enumerate(X):
+            distances = [compute_distance(point, centroid) for centroid in self.centroids]
+            labels[i] = np.argmin(distances)
+        return labels
+
+    def update_centroids(self, X):
+        centroids = np.zeros((self.n_clusters, X.shape[1]))
+        for i in range(self.n_clusters):
+            points = X[self.labels == i]
+            if len(points) > 0:
+                centroids[i] = np.mean(points, axis=0)
+        return centroids
+
+    def predict(self, X):
+        labels = self.assign_clusters(X)
+        return labels
 
 # 使用随机森林分类器对所有样本进行分类
 classifier = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -59,10 +72,10 @@ classes = np.append(train_labels['Class'].unique(), ['PRAD', 'COAD'])
 
 # 使用K均值聚类对未知类别中的样本进行分组
 if not unknown_data.empty:
-    kmeans = KMeans(n_clusters=len(classes), random_state=42)
-    kmeans.fit(train_data)  # 使用训练集数据进行聚类
-    unknown_predictions = kmeans.predict(unknown_data)
-    test_predictions[unknown_indices] = [classes[label] for label in unknown_predictions]
+    kmeans = MyKMeans(n_clusters=len(classes), random_state=42)
+    kmeans.fit(train_data.values)  # 使用训练集数据进行聚类
+    unknown_predictions = kmeans.predict(unknown_data.values)
+    test_predictions[unknown_indices] = [classes[int(label)] for label in unknown_predictions]
 
 # 输出结果
 print("預測結果：")
