0e52e9f11e67efb3d9f1ac2b0adb9f6a4cd73a07
陳育霖_Leo <75311991+wendy062644@users.noreply.github.com>
2024-06-05 20:38:45 +0800

    update

--- FinalTerm/test.py
+++ FinalTerm/test.py
@@ -1,11 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier, IsolationForest
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import KNNImputer
-from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 
-# 加载训练集和测试集数据
 train_data = pd.read_csv('data/train_data.csv', index_col='id')
 train_labels = pd.read_csv('data/train_label.csv', index_col='id')
 test_data = pd.read_csv('data/test_data.csv', index_col='id')
@@ -17,79 +13,89 @@ columns_to_drop = train_data.columns[(train_data == 0).sum() > threshold]
 train_data = train_data.drop(columns=columns_to_drop)
 test_data = test_data.drop(columns=columns_to_drop)
 
-def compute_distance(x1, x2):
+def dis_cal(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
-# 定义K-means算法
-class MyKMeans:
-    def __init__(self, n_clusters, max_iters=100, random_state=42):
-        self.n_clusters = n_clusters
-        self.max_iters = max_iters
-        self.random_state = random_state
+class K_means: # K-means
+    def __init__(self, clusters, iters=100, state=42):
+        self.clusters = clusters
+        self.iters = iters
+        self.state = state
 
-    def fit(self, X):
-        np.random.seed(self.random_state)
-        random_indices = np.random.permutation(X.shape[0])
-        self.centroids = X[random_indices[:self.n_clusters]]
-
-        for _ in range(self.max_iters):
-            self.labels = self.assign_clusters(X)
-            old_centroids = self.centroids
-            self.centroids = self.update_centroids(X)
-
-            if np.all(old_centroids == self.centroids):
-                break
-
-    def assign_clusters(self, X):
+    def cluster_assign(self, X):
         labels = np.zeros(X.shape[0])
         for i, point in enumerate(X):
-            distances = [compute_distance(point, centroid) for centroid in self.centroids]
-            labels[i] = np.argmin(distances)
+            distance = [dis_cal(point, centroid) for centroid in self.centroids]
+            labels[i] = np.argmin(distance)
         return labels
 
-    def update_centroids(self, X):
-        centroids = np.zeros((self.n_clusters, X.shape[1]))
-        for i in range(self.n_clusters):
+    def centroids_update(self, X):
+        centroids = np.zeros((self.clusters, X.shape[1]))
+        for i in range(self.clusters):
             points = X[self.labels == i]
             if len(points) > 0:
                 centroids[i] = np.mean(points, axis=0)
         return centroids
+    
+    def fit(self, X): # 訓練
+        np.random.seed(self.state)
+        indices = np.random.permutation(X.shape[0])
+        self.centroids = X[indices[:self.clusters]]
 
-    def predict(self, X):
-        labels = self.assign_clusters(X)
+        for _ in range(self.iters): # 找中心點
+            self.labels = self.cluster_assign(X)
+            old_cen = self.centroids
+            self.centroids = self.centroids_update(X)
+
+            if np.all(old_cen == self.centroids): # 找到結果，中心沒再改變
+                break
+
+    def predict(self, X): # 預測結果
+        labels = self.cluster_assign(X)
         return labels
 
-# 使用随机森林分类器对所有样本进行分类
 classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 classifier.fit(train_data, train_labels.values.ravel())
 test_predictions_proba = classifier.predict_proba(test_data)
 
-# 定义阈值来确定未知类别的样本
 threshold = 0.75
 
-# 根据概率确定未知类别的样本
 unknown_indices = np.where(np.max(test_predictions_proba, axis=1) < threshold)[0]
 test_predictions = classifier.predict(test_data)
 test_predictions[unknown_indices] = 'Unknown'
+test_labels_array = test_labels.values.ravel()
 
-# 提取未知类别的样本
+a = 0
+b = 0
+c = 0
+
+for i in range(len(test_predictions)):
+    if test_labels_array[i] != 'PRAD' and test_labels_array[i] != 'COAD':
+        if test_predictions[i] == test_labels_array[i]:
+            a += 1
+        else:
+            c += 1
+        b += 1
+
+print(test_predictions)
+print(test_labels_array)
+print(a/b*100, "%")
+print(c/b*100, "%")
+print(a, b, c)
+
 unknown_data = test_data.iloc[unknown_indices]
 
-# 将新增的两个类别名称添加到原有的类别列表中
-classes = np.append(train_labels['Class'].unique(), ['PRAD', 'COAD'])
+classes = ['PRAD', 'COAD']
 
-# 使用K均值聚类对未知类别中的样本进行分组
 if not unknown_data.empty:
-    kmeans = MyKMeans(n_clusters=len(classes), random_state=42)
-    kmeans.fit(train_data.values)  # 使用训练集数据进行聚类
+    kmeans = K_means(2, state=42)
+    kmeans.fit(train_data.values)
     unknown_predictions = kmeans.predict(unknown_data.values)
     test_predictions[unknown_indices] = [classes[int(label)] for label in unknown_predictions]
 
-# 输出结果
 print("預測結果：")
 print(test_predictions)
 
-# 输出与测试标签对比的准确性
 test_labels_array = test_labels.values.ravel()
 accuracy = np.mean(test_predictions == test_labels_array)
 print("\預測正確率：", accuracy)
