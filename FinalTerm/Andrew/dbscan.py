764effe7e26989b284faffeb49492e0f44e67751
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2024-06-05 15:20:56 +0800

    combine into 1 dbscan.py file

--- FinalTerm/Andrew/dbscan.py
+++ FinalTerm/Andrew/dbscan.py
@@ -11,12 +11,84 @@ import io
 from sklearn.ensemble import RandomForestClassifier
 
 # For dbscan algorithm
-from dbscan_helper import *
+from scipy.spatial import distance
 
 # for finding the best hyperparameters
 from sklearn.metrics import silhouette_score
 from bayes_opt import BayesianOptimization
 
+
+class AndrewDBSCANwrapper:
+    # Initialize the DBSCAN object
+    def __init__(self, eps, minimalPts, metric = distance.euclidean):
+        self.eps = eps # eps -> radius of each point to find neighbors
+        self.minimalPts = minimalPts # minimalPts -> minimum number of points to form a cluster
+        self.metric = metric # metric -> distance metric to calculate distance between points
+    
+    def fit_predict(self, data):
+        # create an array of zeros to store cluster labels
+        clusters = [0] * data.shape[0]
+        DBSCAN(data, clusters, self.eps, self.minimalPts, self.metric)
+        return clusters
+
+# DBSCAN algorithm
+def DBSCAN(data, clusters, eps, minimalPts, metric = distance.euclidean):
+    currentPointLabel = 0
+    
+    for point in range(0, data.shape[0]):
+        # if point is already labeled,
+        if clusters[point] != 0: 
+            continue
+    
+        # Find the neighbors of the current point
+        neighbors = neighborsFind(data, point, eps, metric)
+
+        # if point is noise
+        if len(neighbors) < minimalPts:
+            clusters[point] = -1
+
+        else:
+            # keep expanding the cluster 
+            currentPointLabel += 1
+            expandCluster(data, clusters, point, neighbors, currentPointLabel, eps, minimalPts, metric)
+    
+    return clusters
+
+# Find the neighbors of a given point
+def neighborsFind(data, point, eps, metric):
+    neighborsList = []
+    
+    for i in range(data.shape[0]):
+        # If the distance to the point is less than eps, add it to the list of neighbors
+        if metric(data[point], data[i]) < eps:
+            neighborsList.append(i)
+    
+    return neighborsList
+
+# This function expands the cluster from a given `point` until the boundaries of the neighborhood are reached. It assigns the `point` and all points in `neighbors` to the current cluster (`currentPointLabel`).
+def expandCluster(data, clusters, point, neighbors, currentPointLabel, eps, minimalPts, metric):
+    clusters[point] = currentPointLabel
+    
+    i = 0
+    while i < len(neighbors):
+        # Get the next neighbor
+        nextPoint = neighbors[i]
+        # It's still a the cluster point, if the NEXT neighbor is a noise, but no need to find the neighbors of the noise
+        if clusters[nextPoint] == -1: 
+            clusters[nextPoint] = currentPointLabel
+        
+        # Assign the point to the cluster if it hasn't been assigned yet
+        elif clusters[nextPoint] == 0:
+            clusters[nextPoint] = currentPointLabel
+            
+            nextNeighbrs = neighborsFind(data, nextPoint, eps, metric)
+            
+            # if the point is a core point, add its neighbors to the list of neighbors 
+            if len(nextNeighbrs) >= minimalPts:
+                neighbors = neighbors + nextNeighbrs
+        
+        # Move to the next neighbor  
+        i += 1
 
 # Load data
 train_data = pd.read_csv('../Data/train_data.csv', index_col='id')
@@ -169,6 +241,3 @@ for classesis in newclasses: # for finding the best permutation of classes
 print("Test Labels     : ", ' '.join(map(str, test_labels_array)))
 print("Test Predictions: ", ' '.join(map(str, test_predictions.ravel())))
 print("\n預測正確率：", max_accuracy)
-
-
-
