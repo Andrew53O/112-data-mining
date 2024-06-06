47007fad8d71152f54dc45264f3c2b75e638503c
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2024-06-06 23:08:47 +0800

    update test.py

--- FinalTerm/test.py
+++ FinalTerm/test.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
-train_data = pd.read_csv('data/train_data.csv', index_col='id')
-train_labels = pd.read_csv('data/train_label.csv', index_col='id')
-test_data = pd.read_csv('data/test_data.csv', index_col='id')
-test_labels = pd.read_csv('data/test_label.csv', index_col='id')
+train_data = pd.read_csv('Data/train_data.csv', index_col='id')
+train_labels = pd.read_csv('Data/train_label.csv', index_col='id')
+test_data = pd.read_csv('Data/test_data.csv', index_col='id')
+test_labels = pd.read_csv('Data/test_label.csv', index_col='id')
 
 threshold = 0.9 * len(train_data)
 columns_to_drop = train_data.columns[(train_data == 0).sum() > threshold]
@@ -67,21 +67,53 @@ test_labels_array = test_labels.values.ravel()
 
 a = 0
 b = 0
-c = 0
+unknown = 0
+BRCA_test = 0
+KIRC_test = 0
+LUAD_test = 0
+BRCA_predict = 0
+KIRC_predict = 0
+LUAD_predict = 0
+
 
 for i in range(len(test_predictions)):
     if test_labels_array[i] != 'PRAD' and test_labels_array[i] != 'COAD':
+        if test_labels_array[i] == 'BRCA':
+            BRCA_test += 1
+        elif test_labels_array[i] == 'KIRC':
+            KIRC_test += 1
+        elif test_labels_array[i] == 'LUAD':
+            LUAD_test += 1
+            
         if test_predictions[i] == test_labels_array[i]:
             a += 1
+            
+            if test_predictions[i] == 'BRCA':
+                BRCA_predict += 1
+            elif test_predictions[i] == 'KIRC':
+                KIRC_predict += 1
+            elif test_predictions[i] == 'LUAD':
+                LUAD_predict += 1
+           
         else:
-            c += 1
+            unknown += 1
+            
         b += 1
 
-print(test_predictions)
-print(test_labels_array)
+#print(test_predictions)
+#print(test_labels_array)
 print(a/b*100, "%")
-print(c/b*100, "%")
-print(a, b, c)
+print(unknown/b*100, "%")
+print(a, b, unknown)
+print("test data total a b c", b)
+print("BRCA_test", BRCA_test)
+print("KIRC_test", KIRC_test)
+print("LUAD_test", LUAD_test)
+print("BRCA_predict", BRCA_predict)
+print("KIRC_predict", KIRC_predict)
+print("LUAD_predict", LUAD_predict)
+print("uknown", unknown)
+
 
 unknown_data = test_data.iloc[unknown_indices]
 
@@ -99,3 +131,34 @@ print(test_predictions)
 test_labels_array = test_labels.values.ravel()
 accuracy = np.mean(test_predictions == test_labels_array)
 print("\預測正確率：", accuracy)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Values for each group
+group1 = [12, 28, 1]
+group2 = [28, 7, 16, 20]
+
+# Create the x positions of the bars
+x1 = np.arange(len(group1))
+x2 = np.arange(len(group2))
+
+# Create the plot
+fig, ax = plt.subplots()
+
+# # Plot the bars
+# bar_width = 0.35
+# rects1 = ax.bar(x1, group1, bar_width, label='Group 1')
+# rects2 = ax.bar(x2 + len(group1), group2, bar_width, label='Group 2')
+
+# # Add some text for labels, title and custom x-axis tick labels, etc.
+# ax.set_xlabel('Group')
+# ax.set_ylabel('Scores')
+# ax.set_title('Scores by group')
+# ax.set_xticks(np.append(x1, x2 + len(group1)))
+# ax.set_xticklabels(['A', 'B', 'C', 'A_after', 'B_after', 'C_after', 'D_after'])
+# ax.legend()
+
+# # Show the plot
+# plt.show()
+
